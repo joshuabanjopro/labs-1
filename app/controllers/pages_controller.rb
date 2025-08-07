0cfc7831efa503633959490156b422c91e62f484
joshuabanjopro <joshuabanjopro@gmail.com>
2025-08-07 09:39:41 +0100

    developed feature to assign and access comments stored in array against corresponding new heroes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,9 @@ class PagesController < ApplicationController
   def objectives
     @objectives = [ "Defeat Thanos", "Take down Ultron", "Find all of the Infinity Stones", "Put down Wanda..." ]
   end
+
+  def newrecruits
+    @avengers_new_recruits = [ "Spider-Man", "Black Panther", "Ant-Man", "Winter Soldier", "Captain Marvel" ]
+    @avengers_new_recruits_comments = [ "a welcomed addition to the team!", "capable of making us stronger than ever", "everything we've been looking for!", "a much needed bit of muscle", "the GOAT!" ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,5 @@ Rails.application.routes.draw do
 
   get "about", to: "pages#about"
   get "objectives", to: "pages#objectives"
+  get "newrecruits", to: "pages#newrecruits"
 end
