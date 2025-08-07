af02b869dfe0300db2d2c5ba3c13a47aa11020c8
joshuabanjopro <joshuabanjopro@gmail.com>
2025-08-07 09:24:38 +0100

    extended routes and added new controller action and associated view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,8 @@ class PagesController < ApplicationController
     @about_msg = "The Avengers are committed to protectingt all of the multiverses containing Earth..."
     @avengers = [ "Iron Man", "Captain America", "Thor", "Hulk", "Black Widow" ]
   end
+
+  def objectives
+    @objectives = [ "Defeat Thanos", "Take down Ultron", "Find all of the Infinity Stones", "Put down Wanda..." ]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   # root "posts#index"
 
   get "about", to: "pages#about"
+  get "objectives", to: "pages#objectives"
 end
